3da4a2d3041d32b6308a9a8c4c7fdc19fd8b06dc
Piotr Ackermann <pioacker@gmail.com>
2019-11-10 13:51:20 +0100

    Generate canvas

--- src/RayTracer/Canvas.hs
+++ src/RayTracer/Canvas.hs
@@ -1,6 +1,7 @@
 module RayTracer.Canvas 
 (   module Codec.Picture
     , createCanvas
+    , generateCanvas
     , saveCanvas
 )
 where
@@ -16,17 +17,19 @@ type Canvas = Image PixelRGB8
 
 createCanvas :: Int -> Int -> [(Int, Int, Color)] -> Canvas
 createCanvas width height pixels = runST $ do
-    img <- createMutableImage width height black
-    mapM_ (\(x, y, c) -> writePixel img x y (toPixel c)) pixels
-    unsafeFreezeImage img
+        img <- createMutableImage width height black
+        mapM_ (\(x, y, c) -> writePixel img x y (toPixel c)) pixels
+        unsafeFreezeImage img
     where black = PixelRGB8 0 0 0
+
+generateCanvas :: Int -> Int -> (Int -> Int -> Color) -> Canvas
+generateCanvas width height f =
+    generateImage (\x -> toPixel . f x) width height
 
 toPixel :: Color -> PixelRGB8
 toPixel (V3 r g b) = PixelRGB8 (f r) (f g) (f b)
     where f = round . (255*) . clamp 0 1
-
-clamp :: (Ord a) => a -> a -> a -> a
-clamp a b = min b . max a
+          clamp a b = min b . max a
 
 saveCanvas :: FilePath -> Canvas -> IO ()
 saveCanvas filePath = savePngImage filePath . ImageRGB8
--- test/RayTracer/CanvasSpec.hs
+++ test/RayTracer/CanvasSpec.hs
@@ -13,4 +13,9 @@ spec = do
                     c = createCanvas 10 20 [(2, 3, red)]
                 pixelAt c 3 2 `shouldBe` PixelRGB8 0 0 0
                 pixelAt c 2 3 `shouldBe` PixelRGB8 255 0 0
+            it "generates canvas" $ do
+                let cl = color 1 0.8 0.6
+                    cs = generateCanvas 10 20 (\_ -> const cl)
+                pixelAt cs 2 3 `shouldBe` PixelRGB8 255 204 153
+                
 
