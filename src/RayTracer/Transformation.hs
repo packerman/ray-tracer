c4b8348691090e7ece554e3ff6a9f03ba49c08b1
Piotr Ackermann <pioacker@gmail.com>
2019-11-10 21:00:20 +0100

    Scaling

--- src/RayTracer/Transformation.hs
+++ src/RayTracer/Transformation.hs
@@ -7,3 +7,9 @@ translation x y z = matrix 1 0 0 x
                             0 1 0 y
                             0 0 1 z
                             0 0 0 1
+
+scaling :: Double -> Double -> Double -> Matrix
+scaling x y z = matrix x 0 0 0
+                        0 y 0 0
+                        0 0 z 0
+                        0 0 0 1
--- test/RayTracer/TransformationSpec.hs
+++ test/RayTracer/TransformationSpec.hs
@@ -21,3 +21,20 @@ spec = do
                 let transform = translation 5 (-3) 2
                     v = vector (-3) 4 5
                 transform !* v `shouldBe` v
+        describe "Scaling" $ do
+            it "scales point" $ do
+                let transform = scaling 2 3 4
+                    p = point (-4) 6 8
+                transform !* p `shouldBe` point (-8) 18 32
+            it "scales vector" $ do
+                let transform = scaling 2 3 4
+                    p = vector (-4) 6 8
+                transform !* p `shouldBe` vector (-8) 18 32
+            it "inverses scaling" $ do
+                let transform = scaling 2 3 4
+                    v = vector (-4) 6 8
+                inverse transform !* v `shouldBe` vector (-2) 2 2
+            it "reflects point" $ do
+                let transform = scaling (-1) 1 1
+                    p = point 2 3 4
+                transform !* p `shouldBe` point (-2) 3 4
