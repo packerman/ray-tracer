d3a80439a0d4e4e1c82932b9d9101dc1a40be3b7
Piotr Ackermann <pioacker@gmail.com>
2019-12-22 12:49:00 +0100

    Refactor

--- src/RayTracer/Sphere.hs
+++ src/RayTracer/Sphere.hs
@@ -3,13 +3,15 @@ module RayTracer.Sphere where
 import RayTracer.Ray
 import RayTracer.Tuple
 
-intersect :: Ray -> [Double]
-intersect ray = let sphereToRay = origin ray ^-^ point 0 0 0
-                    a = dot (direction ray) (direction ray)
-                    b = 2 * dot (direction ray) sphereToRay
-                    c = (dot sphereToRay sphereToRay) - 1
-                    discriminant = b * b - 4 * a * c
-                in if discriminant < 0 then []
-                    else let t1 = (-b - sqrt discriminant) / (2*a)
-                             t2 = (-b + sqrt discriminant) / (2*a)
-                         in  [t1, t2]
+data Sphere = Sphere
+
+intersect :: Sphere -> Ray -> [Double]
+intersect sphere ray = let sphereToRay = origin ray ^-^ point 0 0 0
+                           a = direction ray `dot` direction ray
+                           b = 2 * (direction ray) `dot` sphereToRay
+                           c = sphereToRay `dot` sphereToRay - 1
+                           discriminant = b * b - 4 * a * c
+                        in if discriminant < 0 then []
+                            else let t1 = (-b - sqrt discriminant) / (2*a)
+                                     t2 = (-b + sqrt discriminant) / (2*a)
+                                    in  [t1, t2]
--- test/RayTracer/SphereSpec.hs
+++ test/RayTracer/SphereSpec.hs
@@ -11,16 +11,21 @@ spec = do
         describe "Ray intersecting a sphere" $ do
             it "may intersect sphere at two points" $ do
                 let r = ray (point 0 0 (-5)) (vector 0 0 1)
-                intersect r `shouldBe` [4, 6]
+                    s = Sphere
+                intersect s r `shouldBe` [4, 6]
             it "may intersect sphere at tangent" $ do
                 let r = ray (point 0 1 (-5)) (vector 0 0 1)
-                intersect r `shouldBe` [5, 5]
+                    s = Sphere
+                intersect s r `shouldBe` [5, 5]
             it "may miss a sphere" $ do
                 let r = ray (point 0 2 (-5)) (vector 0 0 1)
-                intersect r `shouldBe` []
+                    s = Sphere
+                intersect s r `shouldBe` []
             it "may originate inside a sphere" $ do
                 let r = ray (point 0 0 0) (vector 0 0 1)
-                intersect r `shouldBe` [-1, 1]
+                    s = Sphere
+                intersect s r `shouldBe` [-1, 1]
             it "sphere may be behind a ray" $ do
                 let r = ray (point 0 0 5) (vector 0 0 1)
-                intersect r `shouldBe` [-6, -4]
+                    s = Sphere
+                intersect s r `shouldBe` [-6, -4]
