3b710609c6bf5346c1b4cd1d8d30fea744d71381
Piotr Ackermann <pioacker@gmail.com>
2020-05-30 00:09:03 +0200

    Silhouette example

---
+++ example/silhouette.hs
@@ -0,0 +1,37 @@
+import Data.Functor
+import Data.Maybe
+
+import RayTracer.Tuple
+import RayTracer.Canvas
+import RayTracer.Ray
+import RayTracer.Sphere
+import RayTracer.Intersection
+
+rayOrigin = point 0 0 (-5)
+
+wallZ = 10
+
+wallSize = 7 :: Double
+
+canvasPixels = 1200  :: Int
+
+pixelSize = wallSize / fromIntegral canvasPixels
+
+half = wallSize / 2
+
+red = color 1 0 0
+black = color 0 0 0
+
+shape = sphere
+
+canvas = createCanvas canvasPixels canvasPixels
+            ([0..canvasPixels-1] >>= (\y ->
+                let worldY = half - pixelSize * fromIntegral y
+                in [0..canvasPixels-1] <&> (\x ->
+                    let worldX = - half + pixelSize * fromIntegral x
+                        position = point worldX worldY wallZ
+                        r = ray rayOrigin $ normalize $ position ^-^ rayOrigin
+                        xs = intersect sphere r
+                    in (x, y, if (isJust $ hit xs) then red else black))))
+
+main = saveCanvas "silhouette.png" canvas
